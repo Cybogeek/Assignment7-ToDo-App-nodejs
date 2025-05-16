17fb0f63c546efd451538054aade808892c3e441
cybogeek <cybokeek2017@gmail.com>
2025-05-16 16:41:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import methodOverride from "method-override";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// In-memory data store
+let tasks = [];
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index", { tasks });
+});
+
+app.post("/add", (req, res) => {
+  const newTask = req.body.task;
+  if (newTask && newTask.trim().length > 0) {
+    tasks.push(newTask.trim());
+  }
+
+  res.redirect("/");
+});
+
+app.get("/edit/:id", (req, res) => {
+  const taskId = req.params.id;
+  const task = tasks[taskId];
+  res.render("edit", { taskId, task });
+});
+
+app.post("/edit/:id", (req, res) => {
+  const taskId = req.params.id;
+  if (req.body.task && req.body.task.trim().length > 0) {
+    tasks[taskId] = req.body.task.trim();
+  }
+  res.redirect("/");
+});
+
+app.get("/delete/:id", (req, res) => {
+  const taskId = req.params.id;
+  tasks.splice(taskId, 1);
+  res.redirect("/");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ screenshots/Readme.txt
@@ -0,0 +1,20 @@
+***Assignment 7***
+------------------------
+
+
+Steps to Run:
+
+	cd to Project Directory
+
+	bash:
+	node app.js
+	Server is running on http://localhost:3000
+
+
+See Results in Screenshots:
+/screenshots
+	-home.png
+	-entry.png
+	-edit.png
+--------------------------------------
+No Error During Test, Make sure Node is updated.
